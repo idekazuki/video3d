36997b9a14247c0644100ace567d0a57f2fd58ed
idekazuki <kuzikaide@gmail.com>
2019-11-09 17:50:13 +0900

    fix bug

--- img_process_test.py
+++ img_process_test.py
@@ -7,45 +7,34 @@ import cv2
 from read_gulpio import EpicDataset_4_Tmodel
 from gulpio.transforms import Scale, CenterCrop, Compose, UnitNorm
 import numpy as np
-
+import torch
 import matplotlib
 import matplotlib.pyplot as plt
-
-class_counts = (125, 352)
-
-
-batch_size = 1
-segment_count = 8
-snippet_length = 1  # Number of frames composing the snippet, 1 for RGB, 5 for optical flow
-snippet_channels = 3  # Number of channels in a frame, 3 for RGB, 2 for optical flow
-height, width = 224, 224
-
-
-#image proccesing parameter
+from opts import parse_opts
 
 
 
 
-transforms = Compose([
-                     Scale(224),
-                     CenterCrop(224),
-                     ])
-dataset = EpicDataset_4_Tmodel(transforms, class_type='verb', frame_size=8)
+if __name__ =="__main__":
+    opt = parse_opts()
+    opt.class_counts = (125, 352)
+    opt.snippet_length = 1  # Number of frames composing the snippet, 1 for RGB, 5 for optical flow
+    opt.snippet_channels = 3  # Number of channels in a frame, 3 for RGB, 2 for optical flow
 
-for epoch in range(10):
-    for batch_num, (data, label) in enumerate(dataset):
-        data = data.reshape(batch_size, -1, 224, 224)
-        datat = torch.tensor(data, dtype=torch.float32)
-#        result = tsm.features(datat)
-#        verb, noun = tsm.logits(result)
-#        print(verb.shape, noun.shape, torch.argmax(verb, dim=1))
-#        print(verb)
-        img = data[0]
-        img = img.reshape(-1,224,224)
-        img = img.transpose(1,2,0)
-        print(type(img))
-        print(img.shape)
-#        plt.imsave('./img/img{}.png'.format(batch_num * (epoch+1)), img)
+    #image proccesing parameter
+    transforms = Compose([
+                        Scale(opt.cropSsize),
+                        CenterCrop(opt.cropCsize[0]),
+                        ])
+    dataset = EpicDataset_4_Tmodel(transforms, class_type=opt.class_type, frame_size=opt.framesize, path=opt.video_root)
 
-        for i in range(8):
-            plt.imsave('./img/img{}-{}.png'.format(batch_num*(epoch + 1),i),img[:,:,i*3:i*3+3])
+    for epoch in range(opt.epoch_num):
+        for batch_num, (data, label) in enumerate(dataset):
+            data = data.reshape(opt.batch_size, -1, 224, 224)
+            datat = torch.tensor(data, dtype=torch.float32)
+            img = data[0]
+            img = img.reshape(-1,opt.cropCsize[0],opt.cropCsize[0])
+            img = img.transpose(1,2,0)
+
+            for i in range(opt.framesize):
+                plt.imsave('./img/e{}b{}f{}.png'.format(epoch, batch_num, i),img[:,:,i*3:i*3+
