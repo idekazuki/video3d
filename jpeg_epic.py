3e8f7edc28337fc5ce0cb3148b5ffe8508604c5a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-23 06:23:30 +0000

    Adding tarfile member sanitization to extractall()

--- jpeg_epic.py
+++ jpeg_epic.py
@@ -17,6 +17,25 @@ def jpg_open(tar,frame_dist=30):
         yield img
 
 with tarfile.open(dir_name, mode='r') as tar:
-    tar.extractall(members=jpg_open(tar, frame_dist=3000),path='tmp')
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(tar, members=jpg_open(tarframe_dist="3000"), path="tmp")
 
 print('end')
